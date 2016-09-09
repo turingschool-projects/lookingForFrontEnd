92f6ea945f4ba520e0151634d5f38ca57d65df17
Brian Sayler <sayler.b@gmail.com>
2016-09-08 18:40:20 -0600

    adjust server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-var path = require('path');
-var express = require('express');
-
-var app = express();
-var PORT = process.env.PORT || 8080;
+const path = require('path');
+const express = require('express');
+const port = process.env.PORT || 8080;
+let app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/public/index.html');
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 });
 
-app.listen(PORT, function() {
-  console.log("Server is up and running on port: " + PORT)
+app.listen(port, function() {
+  console.log("Server is up and running on port: " + port)
 });
 
 module.exports = app;
