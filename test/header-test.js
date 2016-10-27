0e8134d13593ef209e0bdd9ecb1a5aad2b79ac99
Zack Forbing <zackforbing@gmail.com>
2016-10-27 13:35:58 -0600

    Update header-test.js

--- test/header-test.js
+++ test/header-test.js
@@ -20,7 +20,7 @@ describe('<Header />', () => {
   });
 
   it('has an <Link> with LookingFor text', () => {
-    const wrapper == shallow(<Header/>);
+    const wrapper = shallow(<Header/>);
     expect(wrapper.html()).to.equal(`<div class="nav"><nav class="navbar-fixed-top navbar navbar-default"><div class="container"><div id="brand" class="navbar-header"><a class="navbar-brand">Looking For</a></div></div></nav></div>`);
   });
 });
